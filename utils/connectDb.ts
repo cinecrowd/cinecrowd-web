a5f3529393d01541a51742c8c448bb4658726a95
Farokh Madah <fa.madah@gmail.com>
2024-07-08 05:42:56 +0330

    update db link

--- utils/connectDb.ts
+++ utils/connectDb.ts
@@ -1,9 +1,36 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL!;
+
+interface MongooseConn {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConn = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
 
 export const connectToDb = async () => {
   try {
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI || "");
-    // console.log("Connected to MongoDB");
+    if (cached.conn) return cached.conn;
+
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URL, {
+        dbName: "subscribe",
+        bufferCommands: false,
+        connectTimeoutMS: 30000,
+      });
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     // process.exit(1);
